1f50e98f50c325da8a8f62fbab3768b1ed886bbb
Wyatt <wyatt.wheelerr@gmail.com>
2023-05-08 18:49:44 +1000

    Cleanup code, add multiple images

--- main.c
+++ main.c
@@ -2,38 +2,55 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
-int max(int array[], int n) {
-    int ret = 0;
-
-    for(int i = 0; i < n; i++) {
-        if(array[i] > ret) {
-            ret = array[i];
-        }
-    }
-
-    return ret;
-}
-
-typedef struct
-{
+typedef struct {
     int x, y;
     short life;
     char *name;
 } Man;
 
 typedef struct {
+    int x,y;
+} Triangle;
+
+typedef struct {
     Man man;
+
+    Triangle triangles[100];
 
     // Images
     SDL_Texture *texture;
+
+    SDL_Renderer* renderer;
 } GameState;
 
-void structs() {
-    Man man;
-    man.x = 50;
-    man.y = 50;
-    man.life = 100;
-    man.name = "John";
+void loadGame(GameState* game, SDL_Renderer *renderer) {
+    SDL_Surface *imageSurface = NULL; // src image in memory, texture is rendered image
+
+    int imgFlags = IMG_INIT_PNG;
+
+    if(!(IMG_Init(imgFlags) & imgFlags)) {
+        printf("SDL_image could not initialize. Error %s\n", IMG_GetError());
+    }
+
+    imageSurface = IMG_Load("../triangle.png");
+
+    if(imageSurface == NULL) {
+        printf("SDL_Image could not intialize. Error %s\n", IMG_GetError());
+        SDL_Delay(5000);
+        exit(1);
+    }
+
+    game->texture = SDL_CreateTextureFromSurface(renderer, imageSurface);
+    SDL_FreeSurface(imageSurface);
+
+    game->man.x = 320-40;
+    game->man.y = 240-40;
+
+    // init triangles
+    for(int i = 0; i < 100; i++) {
+        game->triangles[i].x = i*64;
+        game->triangles[i].y = i*32;
+    }
 }
 
 int ProcessEvents(SDL_Window *w, GameState *game) {
@@ -85,25 +102,27 @@ int ProcessEvents(SDL_Window *w, GameState *game) {
     }
 }
 
-void doRender(SDL_Renderer *renderer, GameState* game) {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+void doRender(GameState* game) {
+    SDL_SetRenderDrawColor(game->renderer, 0, 0, 255, 255);
 
     // clear screen to blue
-    SDL_RenderClear(renderer);
+    SDL_RenderClear(game->renderer);
 
     // set draw color to white
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_SetRenderDrawColor(game->renderer, 255, 255, 255, 255);
 
     // draw a rectangle
     SDL_Rect rect = { game->man.x, game->man.y, 100, 100 };
-    SDL_RenderFillRect(renderer, &rect);
+    SDL_RenderFillRect(game->renderer, &rect);
 
     // draw image
-    SDL_Rect imageRect = { 50, 50, 128, 128 };
-    SDL_RenderCopy(renderer, game->texture, NULL, &imageRect);
+    for(int i = 0; i < 100; i++) {
+        SDL_Rect imageRect = { game->triangles[i].x, game->triangles[i].y, 128, 128 };
+        SDL_RenderCopy(game->renderer, game->texture, NULL, &imageRect);
+    }
 
     // done drawing, present to screen
-    SDL_RenderPresent(renderer);
+    SDL_RenderPresent(game->renderer);
 
     //SDL_Delay(10);
 }
@@ -123,8 +142,6 @@ int main(int argc, const char *argv[]) {
     SDL_Window *window; // declare a window
     SDL_Renderer *renderer; // declare a renderer
 
-    SDL_Surface *imageSurface = NULL; // src image in memory, texture is rendered image
-
     SDL_Init(SDL_INIT_VIDEO); // initialize sdl2
 
     Man man;
@@ -134,35 +151,21 @@ int main(int argc, const char *argv[]) {
     // create window
     window = SDL_CreateWindow("Game Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    gameState.renderer = renderer;
 
-    int imgFlags = IMG_INIT_PNG;
-
-    if(!(IMG_Init(imgFlags) & imgFlags)) {
-        printf("SDL_image could not initialize. Error %s\n", IMG_GetError());
-    }
-
-    imageSurface = IMG_Load("../triangle.png");
-
-    if(imageSurface == NULL) {
-        printf("SDL_Image could not intialize. Error %s\n", IMG_GetError());
-        SDL_Delay(5000);
-        return 1;
-    }
-
-    gameState.texture = SDL_CreateTextureFromSurface(renderer, imageSurface);
-    SDL_FreeSurface(imageSurface);
+    loadGame(&gameState, gameState.renderer);
 
     int done = 0;
     while(!done) {
         done = ProcessEvents(window, &gameState);
 
-        doRender(renderer, &gameState);
+        doRender(&gameState);
     }
 
     SDL_DestroyTexture(gameState.texture);
 
     SDL_DestroyWindow(window);
-    SDL_DestroyRenderer(renderer);
+    SDL_DestroyRenderer(gameState.renderer);
 
     SDL_Quit();
 }
